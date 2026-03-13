f650fed07ed9bdc586326c5f93a88702c0073fee
Alexandre Hoffmann <alexandre.hoffmann.etu@gmail.com>
2026-03-13 10:19:54 +0100

    Change BIC::fixed parameter from true to false

--- include/LNOT/GeneralSolvers/SecondOrderSolverBase.hpp
+++ include/LNOT/GeneralSolvers/SecondOrderSolverBase.hpp
@@ -69,7 +69,7 @@ public:
 	 * @param x Output vector for the solution.
 	 */
 	template<CSecondOrderOracle Oracle>
-	void solve(Oracle& oracle, Scalar* x) { CRTP::derived().solveImpl(oracle, BIC::fixed<bool,true>, x); }
+	void solve(Oracle& oracle, Scalar* x) { CRTP::derived().solveImpl(oracle, BIC::fixed<bool,false>, x); }
 	
 	/**
 	 * @brief Solve using a valid SecondOrderOracle with an initial guess.
